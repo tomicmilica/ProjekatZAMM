df8f346d307cd017c2f9b47e97c0b786bb0d7cac
Administrator <Administrator@P05-12>
2017-11-15 10:58:51 +0100

    pocetak

---
+++ Projekat/Projekat/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
