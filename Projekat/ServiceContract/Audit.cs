4cb4549020d695df5902f601bebf8edcf8bc8be1
alisa <Administrator@P05-12>
2017-11-21 10:43:34 +0100

    Audit class

---
+++ Projekat/ServiceContract/Audit.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContract
+{
+    public class Audit
+    {
+        private static EventLog customLog = null;
+        const string SourceName = "Common.Audit";
+        const string LogName = "ReplicatorLog";
+
+        public Audit()
+        {
+            try
+            {
+                if (!EventLog.SourceExists(SourceName))
+                {
+                    EventLog.CreateEventSource(SourceName, LogName);
+                }
+
+                customLog = new EventLog(LogName, Environment.MachineName, SourceName);
+            }
+            catch (Exception e)
+            {
+                customLog = null;
+                Console.WriteLine("Error while trying to create log handle. Error: {0}", e.Message);
+            }
+        }
+
+        public static string Mssg1(string username)
+        {
+            string message = String.Format(AuditEvents.Mssg1, username);
+            return message;
+        }
+
+        public static string Mssg2()
+        {
+            string message = String.Format(AuditEvents.Mssg2);
+            return message;
+        }
+
+        public static string Mssg3()
+        {
+            string message = String.Format(AuditEvents.Mssg3);
+            return message;
+        }
+
+        public static string InitReplication()
+        {
+            string message = String.Format(AuditEvents.InitReplication);
+            return message;
+        }
+
+        public static string ReadReplication()
+        {
+            string message = String.Format(AuditEvents.ReadReplication);
+            return message;
+        }
+    }
+}
---
+++ Projekat/ServiceContract/AuditEvents.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContract
+{
+    public enum AuditEventTypes
+    {
+        Mssg1 = 0,
+        Mssg2 = 1,
+        Mssg3 = 2,
+        InitReplication = 3,
+        ReadReplication = 4
+    }
+
+    public class AuditEvents
+    {
+        private static ResourceManager resourceManager = null;
+        private static object resourceLock = new object();
+
+        private static ResourceManager ResourceMgr
+        {
+            get
+            {
+                lock (resourceLock)
+                {
+                    if (resourceManager == null)
+                    {
+                        resourceManager = new ResourceManager(typeof(Messages).FullName, Assembly.GetExecutingAssembly());
+                    }
+                    return resourceManager;
+                }
+            }
+        }
+
+        public static string Mssg1
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.Mssg1.ToString());
+            }
+        }
+
+        public static string Mssg2
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.Mssg2.ToString());
+            }
+        }
+
+        public static string Mssg3
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.Mssg3.ToString());
+            }
+        }
+
+        public static string InitReplication
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.InitReplication.ToString());
+            }
+        }
+
+        public static string ReadReplication
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.ReadReplication.ToString());
+            }
+        }
+    }
+}
