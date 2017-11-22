a0e333b0fb79aefc8339d4dc37d57b28d9f8fb9e
mirasimicic <mirasimicic@gmail.com>
2017-11-22 15:40:46 +0100

    Obrisala Folder DesAlgorithm i dodala CommonProcess

---
+++ Projekat/ServiceContract/CommonProcess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokusaj
+{
+   abstract class CommonProcess
+    {
+        public abstract string EncryptionStart(string text, string key, bool IsTextBinary);
+
+        public abstract string DecryptionStart(string text, string key, bool IsTextBinary);
+    }
+}
